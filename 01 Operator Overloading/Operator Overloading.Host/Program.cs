ca84c853f6022d165b3640d1892e249fa938b0e3
unknown <ishan@Training11.internal.tavisca.com>
2015-07-07 15:42:18 +0530

    Operator Overloading Committed

---
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Operator_Overloading.Model;
+
+namespace Operator_Overloading.Host
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Money Money1 = new Money();   // Declare Money1 of type Money
+                Money Money2 = new Money();   // Declare Money2 of type Money
+                Money Money3 = new Money();
+
+                Console.WriteLine("Enter currency and amount for first user");
+                Money1.getCurrency(); //recieve currency for first user
+                Money1.getAmount();   //recieve amount for first user
+                Console.WriteLine("Enter currency and amount for second user");
+                Money2.getCurrency(); //recieve currency for second user
+                Money2.getAmount(); //recieve amount for seoond user
+
+
+                Money3 = Money1 + Money2;
+
+                Console.WriteLine("Total Amount : {0} ", Money3.amount); 
+            }
+            catch (ArgumentException e1)
+            {
+                Console.WriteLine(e1);
+            }
+            catch (Exception e2)
+            {
+                Console.WriteLine(e2);
+            }
+            Console.ReadKey();
+           }//end of main
+    }
+}
---
+++ 01 Operator Overloading/Operator Overloading.Model/Money.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading.Model
+{
+    public class Money
+    {
+         public string currency; //recieves currency
+        public double amount;   //recieves amount
+        //overload + operator to add two money objects
+        public void getCurrency()          //to input currency from user
+        {
+            currency = Console.ReadLine();
+            
+        }
+        public void getAmount()          //to input amount from user
+        {
+            
+
+            amount = Convert.ToDouble(Console.ReadLine());
+                if (amount<0)            //validating amount
+                {
+                    throw new ArgumentException("Amount cannot be negative");
+                }
+           
+        }
+         public static Money operator+(Money first,Money second) //operator overloading
+                   { 
+                        if(first.currency==second.currency)
+                        {
+                            Money money = new Money();
+                            money.amount = first.amount + second.amount;
+                            return money;
+                        
+                        }
+                        else
+                        {
+                            throw new System.Exception("Enter Same currency");
+                        }
+
+                      
+                   }
+       
+        
+}
+    }
+
