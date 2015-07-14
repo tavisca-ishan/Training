f4e11ea78818e2c0ad5a92cb90a9fb0359a90d0c
unknown <ishan@Training11.internal.tavisca.com>
2015-07-14 12:18:00 +0530

    Web Server Commit

---
+++ 02WebServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contentPath=ConfigurationManager.AppSettings["contentPath"];
+            HttpServer server = new HttpServer();
+            server.Start(8080,contentPath);
+        }
+    }
+}
---
+++ 02WebServer/RequestParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    public class RequestParser
+    {
+        private Encoding _charEncoder = Encoding.UTF8;
+        public string HttpMethod;
+        public string HttpUrl;
+        public string HttpProtocolVersion;
+
+
+        public void Parser(string requestString)
+        {
+            try
+            {
+                string[] token = requestString.Split(' ');
+                Console.WriteLine(token.Length);
+                token[1] = token[1].Replace("/", "\\");
+                HttpMethod = token[0].ToUpper();
+                HttpUrl = token[1];
+                HttpProtocolVersion = token[2];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                //Console.WriteLine(ex.InnerException.Message);
+                Console.WriteLine("Bad Request");
+            }
+        }
+    }
+}
